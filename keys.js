f154a8df386f45e4a5596b816d6ea04d66bc051f
Jeremy Heyer <jheyer159@gmail.com>
2018-02-03 16:42:47 -0500

    Added firebase keys

---
+++ keys.js
@@ -0,0 +1,10 @@
+export default {
+            firebase: {
+            apiKey: "AIzaSyCbYsPiCPp9BZUEFE_TZQTGCo88s4wnGbk",
+            authDomain: "signupsheet-c8c65.firebaseapp.com",
+            databaseURL: "https://signupsheet-c8c65.firebaseio.com",
+            projectId: "signupsheet-c8c65",
+            storageBucket: "signupsheet-c8c65.appspot.com",
+            messagingSenderId: "45660644322"
+      }
+      }
